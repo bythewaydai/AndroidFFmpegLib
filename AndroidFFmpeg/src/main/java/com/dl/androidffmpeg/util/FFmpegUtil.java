c1b21e914ae5cded01ddcd7925fd292d74776ae3
戴鹿 <manyouzhe@126.com>
2020-06-16 17:45:42 +0800

    更新readme

--- AndroidFFmpeg/src/main/java/com/dl/androidffmpeg/util/FFmpegUtil.java
+++ AndroidFFmpeg/src/main/java/com/dl/androidffmpeg/util/FFmpegUtil.java
@@ -8,15 +8,16 @@ class FFmpegUtil {
      * @param srcFile
      * @param targetFile
      */
-    public static void transformAudio(String srcFile, String targetFile) {
-        FFmpegCmd.execute(FFmpegCommandLineUtil.transformAudio(srcFile, targetFile), new FFmpegCmd.OnHandleListener() {
-            @Override
-            public void onBegin() {
-            }
-
-            @Override
-            public void onEnd(int resultCode, String resultMsg) {
-            }
-        });
+    public static void transformAudio(String srcFile, String targetFile, FFmpegCmd.OnHandleListener onHandleListener) {
+        FFmpegCmd.execute(FFmpegCommandLineUtil.transformAudio(srcFile, targetFile), onHandleListener);
+//        FFmpegCmd.execute(FFmpegCommandLineUtil.transformAudio(srcFile, targetFile), new FFmpegCmd.OnHandleListener() {
+//            @Override
+//            public void onBegin() {
+//            }
+//
+//            @Override
+//            public void onEnd(int resultCode, String resultMsg) {
+//            }
+//        });
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # AndroidFFmpegLib
+
+简单介绍：Android平台下FFmpeg常用命令的使用工具库。
+暂时有这两个类FFmpegCmd，FFmpegCommandLineUtil（具体可看源码）。
+
+FFmpegCommandLineUtil：包含了各种FFmpeg命令的组合
+FFmpegCmd：执行FFmpeg的命令并给出执行结果的回调
+
+####引用方式：
+在“Project”的build.gradle中添加以下仓库
+```javascript
+allprojects {
+    	repositories {
+			...
+			maven { url 'https://jitpack.io' }
+		}
+	}
+```
+
+在“app”的build.gradle中添加引用
+```javascript
+dependencies {
+            implementation 'com.github.bythewaydai:AndroidFFmpegLib:master-SNAPSHOT'
+	}
+```
